e0fbc3eaf97382042d741fdf9d706675919b37c0
WilbertAudrey <112563155+WilbertAudrey@users.noreply.github.com>
2023-05-06 22:37:12 +0700

    Add files via upload

--- learning/learning_app/models.py
+++ learning/learning_app/models.py
@@ -22,7 +22,6 @@ class TableResto(models.Model):
     code = models.CharField(max_length= 200)
     name = models.CharField(max_length= 100)
     capacity = models.IntegerField(default = 0)
-    table_status= models.CharField(max_length= 15, choices = status_table_choices, default= 'Kosong')
     status = models.CharField(max_length=15, choices= status_choices, default= 'Aktif')
     user_create = models.ForeignKey(User, related_name= 'user_create_table_resto', blank = True, null = True, on_delete= models.SET_NULL)
     user_create = models.ForeignKey(User, related_name= 'user_update_table_resto', blank = True, null = True, on_delete= models.SET_NULL)
