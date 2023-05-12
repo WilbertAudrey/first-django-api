0ca0d4ca226f4559c6ec4a16b52e812cf3e41234
WilbertAudrey <112563155+WilbertAudrey@users.noreply.github.com>
2023-05-12 23:24:47 +0700

    testing

--- learning/api/serializers.py
+++ learning/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from learning_app.models import (
-    User, Topic,
+    User, Lesson,Category, LessonDetail
 )
 from django.contrib.auth import authenticate
 from rest_framework import exceptions
@@ -9,8 +9,18 @@ from django.http import HttpRequest, JsonResponse
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
-class TopicSerializer(serializers.ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Topic
-        fields = ('id', 'Code', 'Name', 'status')
+        model = Lesson
+        fields = ('id', 'Lesson_code', 'Lesson_name', 'Lesson_description','status')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'Category_code', 'Category_name', 'Category_description')
+
+class LessonDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LessonDetail
+        fields = ('id', 'nama_subjudul', 'Lesson_name', 'isi')
 
